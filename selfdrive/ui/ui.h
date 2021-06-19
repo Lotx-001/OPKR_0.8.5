3be417404d05627fdb7f2a05f5d4429d6457a31b
jungch <jungch@naver.com>
2021-06-19 21:20:36 +0900

    Hoya Update : fix

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -82,7 +82,7 @@ typedef enum UIStatus {
   STATUS_CRUISE,
 } UIStatus;
 
-static std::map<UIStatus, NVGcolor> bg_colors = {
+const QColor bg_colors [] = {
   {STATUS_DISENGAGED, nvgRGBA(0x17, 0x33, 0x49, 0xc8)},
   {STATUS_ENGAGED, nvgRGBA(0x17, 0x86, 0x44, 0xf1)},
   {STATUS_WARNING, nvgRGBA(0xDA, 0x6F, 0x25, 0xf1)},
